e52125ccb14a7f61e5e8f9d9f9b7d3885d6b8be4
00008420-collab <00008420@uca.edu.sv>
2025-11-24 02:05:23 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Proyecto SGI", page_icon="📋")
+
+st.title("✔ La app ha iniciado correctamente")
+st.write("Si ves este mensaje, el despliegue en Streamlit fue exitoso.")
+st.write("Ahora podemos integrar tus 19 CRUDs sin errores.")
