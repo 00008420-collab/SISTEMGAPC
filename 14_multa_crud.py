c462e6815b688253f97cf1f0802554f4284c07d1
00008420-collab <00008420@uca.edu.sv>
2025-11-24 09:43:20 -0600

    Delete 14_multa_crud.py

--- 14_multa_crud.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from crud_template import render_crud
-table="multa"
-pk="id_multa"
-cols=["id_multa","id_miembro","tipo","monto","descripcion","fecha","estado"]
-render_crud(table, pk, cols)
