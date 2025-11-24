9303590a4467076f6d37c41df5d2aebceb987e77
00008420-collab <00008420@uca.edu.sv>
2025-11-24 09:39:19 -0600

    Delete 07_ciclo_crud.py

--- 07_ciclo_crud.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from crud_template import render_crud
-table="ciclo"
-pk="id_ciclo"
-cols=["id_ciclo","fecha_inicio","fecha_fin","estado","total_utilidad"]
-render_crud(table, pk, cols)
